ecc90e4588a4ee072f850b35fa38c695799c475d
nlaramee120 <nlaramee120@aol.com>
2022-01-10 18:03:32 -0600

    Created the resolvers and typeDefs files, and implemented them into the server.

--- server/server.js
+++ server/server.js
@@ -2,10 +2,21 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+
 const { typeDefs, resolvers } = require('./schemas')
+
+const { authMiddleware } = require('./utils/auth')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new apolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
+
+server.applyMiddleware({ app })
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
